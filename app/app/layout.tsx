d2c9bd9a94d59c532173a5e9c0f8742f32525409
Abdallah <abdallah.moubarak02@gmail.com>
2022-12-02 03:42:11 +0200

    WM-3 adding react-query to nextjs 13

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/styles/globals.css";
+import ReactQueryWrapper from "./ReactQueryWrapper";
+
+type Props = { children: React.ReactNode };
+
+export default function RootLayout({ children }: Props) {
+  console.log("performance");
   return (
     <html lang="en" dir="ltr">
-      <body>{children}</body>
+      <body>
+        <ReactQueryWrapper>{children}</ReactQueryWrapper>
+      </body>
     </html>
   );
 }
