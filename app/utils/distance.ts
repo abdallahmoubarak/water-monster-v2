81505fce303d735d37909ee9b23a6ee48de1c852
Abdallah <abdallah.moubarak02@gmail.com>
2022-12-04 17:41:46 +0200

    WM-6 changing distance types

--- app/utils/distance.ts
+++ app/utils/distance.ts
@@ -1,4 +1,10 @@
-export default function getTimeDistance({ currentLocation, distanceLocation }) {
+export default function getTimeDistance({
+  currentLocation,
+  distanceLocation,
+}: {
+  currentLocation: number[];
+  distanceLocation: { longitude: number; latitude: number };
+}) {
   const distance =
     Math.abs(currentLocation[0] - distanceLocation?.longitude) +
     Math.abs(currentLocation[1] - distanceLocation?.latitude);
