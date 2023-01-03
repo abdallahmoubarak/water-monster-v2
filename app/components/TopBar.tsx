ff8479a9aaae957da9bc94393c70579cd3a60fd1
Abdallah <abdallah.moubarak02@gmail.com>
2023-01-03 22:23:57 +0200

    WM-18 updating top bar

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,40 +1,18 @@
-import { styles } from "@/utils/styles";
 import Logo from "./svg/Logo";
 
 export default function TopBar({ hasImg, setActive }: topBarProps) {
   return (
     <>
-      <div className="top-bar">
-        <div className="logo">
+      <div className="flex items-center justify-between gap-4 bg-primary px-4 py-2 h-16">
+        <div className="h-full">
           <Logo />
         </div>
         {hasImg && (
           <div
-            className="profile-image"
+            className="dark-profile-background w-12 h-12 cursor-pointer rounded-full"
             onClick={() => !!setActive && setActive("Profile")}></div>
         )}
       </div>
-
-      <style jsx>{`
-        .top-bar {
-          background: ${styles.primaryColor};
-          padding: 0.6rem 1.2rem;
-          height: 4rem;
-          ${styles.flexAligncenter};
-          ${styles.justifyBetween};
-          gap: 1rem;
-        }
-        .logo {
-          height: 100%;
-        }
-        .profile-image {
-          width: 3rem;
-          height: 3rem;
-          cursor: pointer;
-          ${styles.borderRadius50percent};
-          ${styles.darkProfileBackground};
-        }
-      `}</style>
     </>
   );
 }
