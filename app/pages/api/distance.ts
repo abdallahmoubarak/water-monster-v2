ab5ff6611169edabbb0e63ff0e79f299124f583c
Abdallah <abdallah.moubarak02@gmail.com>
2023-02-02 22:42:51 +0200

    WM-1 fixing endpoint issue

--- app/pages/api/distance.ts
+++ app/pages/api/distance.ts
@@ -1,6 +1,16 @@
 import { GraphQLClient } from "graphql-request";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+declare const process: {
+  env: {
+    NEXT_PUBLIC_GQL_PATH: string;
+    NEXT_PUBLIC_BASEURL: string;
+  };
+};
+
+const endpoint: string =
+  process.env.NEXT_PUBLIC_BASEURL + process.env.NEXT_PUBLIC_GQL_PATH;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -11,12 +21,7 @@ export default async function handler(
   };
 
   if (distance && serialNumber) {
-    const graphQLClient = new GraphQLClient(
-      process.env.NEXT_PUBLIC_ENDPOINT || "",
-      {
-        mode: "cors",
-      },
-    );
+    const graphQLClient = new GraphQLClient(endpoint, { mode: "cors" });
 
     await graphQLClient.request(
       `mutation {
