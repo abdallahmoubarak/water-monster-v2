ce282c47549c54901501a0cb06b05ad0a415bb96
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-02-11 01:07:08 +0200

    WM-1 updating steps process frontend

---
+++ app/components/ToolTipButton.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { MdOutlineContentCopy } from "react-icons/md";
+
+const password = "oiafdjoih2380jofisa";
+export default function ToolTipButton() {
+  const [isTooltipVisible, setTooltipVisibility] = useState(false);
+
+  const handleButtonClick = () => {
+    setTooltipVisibility(true);
+    setTimeout(() => {
+      setTooltipVisibility(false);
+    }, 2000);
+  };
+  return (
+    <div className="flex gap-1 items-center flex-wrap">
+      <div className="py-4">
+        <button
+          className="flex gap-1 justify-center self-center border border-black p-1 rounded cursor-pointer bg-white w-56"
+          data-tooltip-target="tooltip-default"
+          onClick={() => {
+            navigator.clipboard.writeText(password);
+            handleButtonClick();
+          }}
+        >
+          {password} <MdOutlineContentCopy />
+        </button>
+      </div>
+
+      <div
+        id="tooltip-default"
+        role="tooltip"
+        className={`  ${
+          isTooltipVisible ? "visible" : "invisible"
+        }  px-2 py-1 text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-100 tooltip dark:bg-gray-700`}
+      >
+        Copied
+      </div>
+    </div>
+  );
+}
--- app/pages/installation.tsx
+++ app/pages/installation.tsx
@@ -5,14 +5,20 @@ import Image from "next/image";
 import Button from "@/components/Button";
 import { useEffect, useState } from "react";
 import { QrReader } from "react-qr-reader";
+import ToolTipButton from "@/components/ToolTipButton";
 
 export default function Home() {
   const router = useRouter();
   const [step, setStep] = useState(1);
   const [data, setData] = useState("No QR code detected");
+  const [isScan, setIsScan] = useState(false);
+
   useEffect(() => {
-    console.log(data);
-  }, []);
+    if (data !== "No QR code detected") {
+      setStep(3);
+      setIsScan(false);
+    }
+  }, [data]);
   return (
     <>
       <Head>
@@ -21,16 +27,25 @@ export default function Home() {
       <main>
         <Layout title={"Installation"} onClick={() => router.replace("/")}>
           <h1 className="text-3xl pt-6">Step {step}</h1>
+          <div className="text-center py-2">
+            {step === 1 && "Plug the Water Monster hub to electricity"}
+            {step === 2 && "Scan the QR code placed on the card inside the box"}
+            {step === 3 && "Setup your wifi configuration"}
+            {step === 4 && "Inserting container's info, comming soon..."}
+          </div>
           {step === 1 && (
-            <Image
-              className="mx-auto py-8"
-              src="/gif/plugin.gif"
-              alt=""
-              width={300}
-              height={300}
-            />
+            <>
+              <Image
+                className="mx-auto py-8"
+                src="/gif/plugin.gif"
+                alt=""
+                width={300}
+                height={300}
+              />
+              <Button text={"Next"} onClick={() => setStep(2)} />
+            </>
           )}
-          {step === 2 && (
+          {step === 2 && !isScan && (
             <>
               <Image
                 className="mx-auto py-8"
@@ -39,24 +54,10 @@ export default function Home() {
                 width={300}
                 height={300}
               />
+              <Button text={"Scan Now"} onClick={() => setIsScan(true)} />
             </>
           )}
-          {step === 3 && (
-            <Image
-              className="mx-auto py-8"
-              src="/gif/wifi.gif"
-              alt=""
-              width={300}
-              height={300}
-            />
-          )}
-          <div className="text-center py-8">
-            {step === 1 && "Plug the Water Monster hub to electricity"}
-            {step === 2 && "Scan the QR code placed on the card inside the box"}
-            {step === 3 && "Setup your wifi configuration"}
-            {step === 4 && "Inserting container's info, comming soon..."}
-          </div>
-          {step === 2 && (
+          {isScan && (
             <>
               <QrReader
                 onResult={(result, error) => {
@@ -75,15 +76,33 @@ export default function Home() {
                   facingMode: "environment",
                 }}
               />
-              <p className="pb-6">{data}</p>
+              <div className="mx-auto">{data}</div>
             </>
           )}
-          <Button
-            text={"Done"}
-            onClick={() =>
-              step < 4 ? setStep((step) => step + 1) : router.replace("/")
-            }
-          />
+          {step === 3 && (
+            <>
+              <Image
+                className="mx-auto py-2"
+                src="/gif/wifi.gif"
+                alt=""
+                width={300}
+                height={300}
+              />
+              <div className="px-2">
+                <ul className="mx-auto">
+                  <li>1- Copy the password below.</li>
+                  <li>2- Go to the wifi manager.</li>
+                  <li>
+                    3- Connect to
+                    <span className="color-primary">Water-Monster</span> SSID.
+                  </li>
+                  <li>4- Configure your wifi.</li>
+                </ul>
+                <ToolTipButton />
+              </div>
+              <Button text={"Done"} onClick={() => router.push("/")} />
+            </>
+          )}
         </Layout>
       </main>
     </>
