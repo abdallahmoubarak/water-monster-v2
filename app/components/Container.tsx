8403bf815eb3e19b770d3817a267c141d44c401c
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-02-16 19:25:59 +0200

    WM-1 last done

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -28,7 +28,8 @@ export default function Container({
   };
 
   const calc =
-    ((container?.height - container?.distance - 192) * 100) / container?.height;
+    ((container?.height - (container?.distance - 192)) * 100) /
+    container?.height;
 
   const waterLevel = calc > 0 && calc < 100 ? calc : calc < 0 ? 1 : 100;
 
