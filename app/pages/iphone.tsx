794b70ccf9a42b8661f32c316837c6fc1b5aa604
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-04-26 08:26:00 +0300

    input issue in iphone

--- app/pages/iphone.tsx
+++ app/pages/iphone.tsx
@@ -1,23 +1,13 @@
 import Button from "@/components/Button";
-import Input from "@/components/Input";
-import InputsContainer from "@/components/InputsContainer";
-import Select from "@/components/Select";
 import { validSign } from "@/utils/signValidation";
-import { MouseEventHandler, useState } from "react";
-import { styles } from "@/utils/styles";
-import { useSignIn, useSignUp } from "@/hooks/useAuth";
+import { useState } from "react";
+import { useSignIn } from "@/hooks/useAuth";
 
-export default function Iphone({
-  handleForget,
-}: {
-  handleForget: MouseEventHandler<HTMLButtonElement>;
-}) {
+export default function Iphone() {
   const [email, setEmail] = useState<string>("");
   const [password, setPass] = useState<string>("");
   const [msg, setMsg] = useState<string>("");
-  const [selectedType, setSelectedType] = useState<string>("Client");
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [invalid, setInvalid] = useState<boolean>(false);
 
   const { mutate: signIn } = useSignIn({ setMsg, setIsLoading });
 
@@ -25,7 +15,7 @@ export default function Iphone({
     if (!isLoading) {
       setIsLoading(true);
       setMsg("");
-      let userType = selectedType;
+      let userType = "Client";
       let newEmail = email.toLowerCase().trim();
       const { valid, message } = validSign({
         signType,
@@ -36,19 +26,18 @@ export default function Iphone({
 
       if (!valid) {
         setIsLoading(false);
-        setInvalid(true);
         return setMsg("*" + message);
       }
-      setInvalid(false);
       setMsg(message);
 
       signIn({ email: newEmail, password });
     }
   };
   return (
-    <>
+    <div className="flex flex-col gap-2">
       <input
         type="text"
+        className="border border-black"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -56,6 +45,7 @@ export default function Iphone({
       <input
         type="password"
         placeholder="Password"
+        className="border border-black"
         value={password}
         onChange={(e) => setPass(e.target.value)}
       />
@@ -65,6 +55,6 @@ export default function Iphone({
         isLoading={isLoading}
         onClick={() => handleSignClick("signin")}
       />
-    </>
+    </div>
   );
 }
