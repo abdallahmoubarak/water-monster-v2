6c6647290b8084d24ec6830591c2558691f789bb
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-05-14 01:31:52 +0300

    fixing disabled style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,12 +12,12 @@ export default function Button({
     <button
       onClick={onClick}
       className={` py-2 px-8 rounded-full text-lg mx-auto block w-fit min-w-[9rem] border cursor-pointer ${
-        isSecondary
+        disabled
+          ? "text-white bg-gray-400 border-gray-400 hover:bg-gray-400"
+          : isSecondary
           ? "bg-white text-secondary border-secondary hover:bg-secondary hover:text-white"
           : "border-primary bg-primary text-white hover:bg-blue-400"
-      } ${
-        disabled && "text-white bg-gray-400 border-gray-400 hover:bg-gray-400"
-      }`}
+      } `}
     >
       {isLoading ? (
         <div className="w-[1rem] h-6 mx-auto flex justify-center items-center">
