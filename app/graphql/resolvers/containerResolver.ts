c34bf408dfc28c504717002395d59cc65dbb6fac
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-12-15 11:19:52 +0200

    updating container resolver

--- app/graphql/resolvers/containerResolver.ts
+++ app/graphql/resolvers/containerResolver.ts
@@ -14,6 +14,9 @@ export const containerMutations = {
   ) => {
     const session = driver.session();
     const existingContainers = await session.run(
+      `MATCH (c:Container {serialNumber:"${serialNumber}"}) RETURN c`,
+    );
+    const owners = await session.run(
       `MATCH (owner)-[:OWNS]->(c:Container {serialNumber:"${serialNumber}"}) RETURN c,owner`,
     );
     const [existingContainer]: any = existingContainers.records.map(
@@ -24,13 +27,13 @@ export const containerMutations = {
     const [user]: any = users.records.map(
       (record) => record.get("u").properties,
     );
-    const [owner]: any = existingContainers.records.map(
+    const [owner]: any = owners.records.map(
       (record) => record.get("owner").properties,
     );
 
     if (!user) throw new Error(`User with id ${userId} does not exist!`);
 
-    if (!existingContainer) {
+    if (!existingContainer && !owner) {
       // create new container with user as owner
       const id = uuidv4();
       const newContainer = await session.run(`
@@ -46,17 +49,22 @@ export const containerMutations = {
       );
 
       return container;
+    } else if (!owner && existingContainer) {
+      await session.run(`
+      MATCH (c:Container {serialNumber: "${serialNumber}"})
+      MATCH (u:User {  id: "${userId}" })
+      MERGE (c)<-[:OWNS]-(u)
+    `);
+      return existingContainer;
     } else {
       // update existing container with user as viewer
-      if (owner?.id === userId) {
-        return existingContainer;
-      }
+      if (owner?.id === userId) return existingContainer;
+
       await session.run(`
         MATCH (c:Container {serialNumber: "${serialNumber}"})
         MATCH (u:User {  id: "${userId}" })
         MERGE (c)<-[:CAN_VIEW]-(u)
       `);
-
       return existingContainer;
     }
   },
