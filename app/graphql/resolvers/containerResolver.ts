129718f2315ae7b9dce6904a7479fba223634c6f
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-12-15 11:01:59 +0200

    updating containerResolver

--- app/graphql/resolvers/containerResolver.ts
+++ app/graphql/resolvers/containerResolver.ts
@@ -10,19 +10,22 @@ type containerTypes = {
 export const containerMutations = {
   createOrUpdateContainer: async (
     _source: any,
-    { serialNumber, userId, location }: containerTypes
+    { serialNumber, userId, location }: containerTypes,
   ) => {
     const session = driver.session();
     const existingContainers = await session.run(
-      `MATCH (c:Container {serialNumber:"${serialNumber}"}) RETURN c`
+      `MATCH (owner)-[:OWNS]->(c:Container {serialNumber:"${serialNumber}"}) RETURN c,owner`,
     );
     const [existingContainer]: any = existingContainers.records.map(
-      (record) => record.get("c").properties
+      (record) => record.get("c").properties,
     );
 
     const users = await session.run(`MATCH (u:User {id:"${userId}"}) RETURN u`);
     const [user]: any = users.records.map(
-      (record) => record.get("u").properties
+      (record) => record.get("u").properties,
+    );
+    const [owner]: any = existingContainers.records.map(
+      (record) => record.get("owner").properties,
     );
 
     if (!user) throw new Error(`User with id ${userId} does not exist!`);
@@ -39,12 +42,15 @@ export const containerMutations = {
       `);
 
       const [container]: any = newContainer.records.map(
-        (record) => record.get("c").properties
+        (record) => record.get("c").properties,
       );
 
       return container;
     } else {
       // update existing container with user as viewer
+      if (owner?.id === userId) {
+        return existingContainer;
+      }
       await session.run(`
         MATCH (c:Container {serialNumber: "${serialNumber}"})
         MATCH (u:User {  id: "${userId}" })
