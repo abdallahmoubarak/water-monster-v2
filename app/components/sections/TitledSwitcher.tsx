e18fa63f5b3910fdad93e836efd741567c87bfce
hussein23456 <104328735+hussein23456@users.noreply.github.com>
2023-12-16 18:50:10 +0200

    edits

--- app/components/sections/TitledSwitcher.tsx
+++ app/components/sections/TitledSwitcher.tsx
@@ -4,20 +4,20 @@ import { useEffect, useState } from "react";
 export default function TitledSwitcher({ title }: { title: string }) {
   const [toggle, setToggle] = useState(false);
   const [registration, setRegistration] = useState<any>();
-  // useEffect(() => {
-  //   const registerServiceWorker = async () => {
+  useEffect(() => {
+    const registerServiceWorker = async () => {
   
-  //       try {
-  //         const registration = await navigator?.serviceWorker?.register("../../sw.js");
-  //         console.log("Service Worker registered with scope:", registration.scope);
-  //       } catch (error) {
-  //         console.error("Service Worker registration failed:", error);
+        try {
+          const registration = await navigator?.serviceWorker?.register("../../sw.js");
+          console.log("Service Worker registered with scope:", registration.scope);
+        } catch (error) {
+          console.error("Service Worker registration failed:", error);
         
-  //     }
-  //   };
+      }
+    };
 
-  //   registerServiceWorker();
-  // }, []);
+    registerServiceWorker();
+  }, []);
   const handlePush = () => {
     setToggle(!toggle);
     
