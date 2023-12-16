54183dc0cd8c6c595b0331cbe7b838bd45fbe1c1
hussein23456 <104328735+hussein23456@users.noreply.github.com>
2023-12-16 19:20:14 +0200

    use fetch api test

--- app/utils/SendFcm.ts
+++ app/utils/SendFcm.ts
@@ -1,32 +1,28 @@
-import axios from 'axios'
-export const handleSendFCM = async (fcmToken:any,title:any,body:any) => {
+ export const handleSendFCM = async (fcmToken:any,title:any,body:any) => {
    
     try {
  
      
 
-        const apiUrl = 'https://fcm.googleapis.com/fcm/send';
-
-        const headers = {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer AAAAqwaYmvg:APA91bH96Zkyw4YGV_SY7VlF5nQZZlG_nzSpFu2XIrHhX7fr7kdkyDCqpY8Quba2w07570T07m0No3QEzSd5l4uhvqRB4ypMjqIgSiRHVRtaRDfMsKvlhvpUVinCz2iT6eceMfV2kAId',
-        };
-        
-        const data = {
-          to: fcmToken,
+      // Send a test FCM payload to the current token
+      const response = await fetch('https://fcm.googleapis.com/fcm/send', {
+        method: 'POST',
+        // timeout: 1000,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer AAAAqwaYmvg:APA91bH96Zkyw4YGV_SY7VlF5nQZZlG_nzSpFu2XIrHhX7fr7kdkyDCqpY8Quba2w07570T07m0No3QEzSd5l4uhvqRB4ypMjqIgSiRHVRtaRDfMsKvlhvpUVinCz2iT6eceMfV2kAId`
+        },
+        body: JSON.stringify({
+          to:fcmToken,
           notification: {
             title: title,
             body: body,
           },
-        };
-        
-        axios.post(apiUrl, data, { headers })
-          .then(response => {
-            console.log(response.data);
-          })
-          .catch(error => {
-            console.error(error);
-          });
+        }),
+      });
+ 
+      const responseData = await response.json();
+      console.log(responseData);
       
       alert("Notification sent Successfully!")
      
