76b12427bb7d135b1daad513f419fc873f52c1f9
hussein23456 <104328735+hussein23456@users.noreply.github.com>
2023-12-16 11:51:14 +0200

    edit

--- app/public/firebase-messaging-sw.js
+++ app/public/firebase-messaging-sw.js
@@ -32,20 +32,3 @@ messaging.onBackgroundMessage((payload) => {
 });
 
 // Foreground message handler
-messaging.onMessage((payload) => {
-  
-  console.log('[firebase-messaging-sw.js] Received foreground message', payload);
-
-  // Handle the foreground message as needed
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    // icon: './android-chrome-192x192.png',
-  };
-  const notification = new Notification('New Message', {
-    body: payload?.notification?.body,
-  });
-
-  // Display a notification or update the UI based on the payload
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
