465a931dd680bd29b060ca4eb19c03d73b18f1b8
hussein23456 <104328735+hussein23456@users.noreply.github.com>
2023-12-29 12:55:04 +0200

    edits

--- app/components/pages/Containers.tsx
+++ app/components/pages/Containers.tsx
@@ -17,40 +17,46 @@ export default function Containers({
   setCurrentContainer: Function;
 }) {
   const [alertMsg, setAlertMsg] = useState<string>("");
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   let currentUser: any = localStorage.getItem("User");
   currentUser =
     (currentUser && JSON.parse(currentUser)) ||
     client.getQueryData<userTypes>(["User"]);
-    const handleRefresh = () => {
-      // Handle refresh logic here
-     alert("refresh")
+
+  const handleRefresh = () => {
+    // Simulate async refresh logic
+    setIsRefreshing(true);
+    alert("Refreshing...");
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 1500);
+  };
+
+  useEffect(() => {
+    const onScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      // Adjust the threshold as needed
+      const scrollThreshold = 20;
+
+      if (scrollY + windowHeight >= documentHeight - scrollThreshold && !isRefreshing) {
+        // Trigger refresh when reaching the bottom with a buffer (threshold)
+        handleRefresh();
+      }
     };
-  
-    useEffect(() => {
-      const onScroll = () => {
-        const scrollY = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const documentHeight = document.documentElement.scrollHeight;
-  
-        // Adjust the threshold as needed
-        const scrollThreshold = 100;
-  
-        if (scrollY + windowHeight >= documentHeight - scrollThreshold) {
-          // Trigger refresh when reaching the bottom with a buffer (threshold)
-          handleRefresh();
-        }
-      };
-  
-      // Attach the scroll event listener
-      window.addEventListener('scroll', onScroll);
-  
-      // Clean up the event listener on component unmount
-      return () => {
-        window.removeEventListener('scroll', onScroll);
-      };
-    }, []); // Ensure the effect runs only once on mount
-  
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', onScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [isRefreshing]); // Ensure the effect runs whenever isRefreshing changes
+
   const {
     data: containers,
     isLoading,
@@ -73,6 +79,26 @@ export default function Containers({
     <>
       <ServicesBar setPage={setPage} />
       <div className="p-3">
+        {isRefreshing && (
+          <div
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '50px',
+              background: '#eee',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: '18px',
+              fontWeight: 'bold',
+            }}
+          >
+            Refreshing...
+          </div>
+        )}
+
         {isLoading && <ContainerLoader />}
         <div className="flex items-center justify-center gap-4 pb-4 flex-wrap ">
           {containers?.map((container: any, i: Key) => (
