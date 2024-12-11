93a2ef4d23ada4c26a5b930e7c9ee7e742d525bc
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-12-11 15:30:27 +0300

    fixing width

--- app/components/atoms/TopBar.tsx
+++ app/components/atoms/TopBar.tsx
@@ -4,13 +4,14 @@ export default function TopBar({ hasImg, setActive }: topBarProps) {
   return (
     <>
       <div className="flex items-center justify-between gap-4  bg-primary px-4 py-2 h-16">
-        <div className="h-full">
+        <div className="h-full w-fit">
           <Logo />
         </div>
         {hasImg && (
           <div
             className="dark-profile-background w-12 h-12 cursor-pointer rounded-full"
-            onClick={() => !!setActive && setActive("Profile")}></div>
+            onClick={() => !!setActive && setActive("Profile")}
+          ></div>
         )}
       </div>
     </>
